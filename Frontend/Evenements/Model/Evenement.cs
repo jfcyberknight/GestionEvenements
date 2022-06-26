6c12ccb58883420b5d37b63c1459e0fe343a34c7
Vallée Jean-François <vallee.jeanfrancois@gmail.com>
2022-06-26 17:14:04 -0400

    frontend connecté au backend (hardcodé)

---
+++ Frontend/Evenements/Model/Evenement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Frontend.Evenements.Model
+{
+    public partial class Evenement
+    {
+        public long Id { get; set; }
+        public string? Nom { get; set; }
+        public string? Description { get; set; }
+        public DateTime? Datedebut { get; set; }
+        public DateTime? Datefin { get; set; }
+    }
+}
